d7de835dc9bf73f591ea00491e8265c9f3c99031
Matheus Woeffel <matheuswoeffel@gmail.com>
2019-06-15 22:20:21 -0300

    Implementação bitmap

--- src/bitmap.c
+++ src/bitmap.c
@@ -28,12 +28,10 @@ static void initializeBitmapInfo(){
     
     //Calcula o tamanho do bitmap em setores.
     bitmapInfo.bitmapSize = ceil((float)(bitmapInfo.indexBitmapSize + bitmapInfo.dataBitmapSize) / (float)SECTOR_SIZE * 8);
-
     bitmapInfo.bitmapStart = partInfo.firstSectorAddress + 1;
 }
 
 static	bool getBit (int bitNumber, BYTE *cache) {
-	
 	int	byteAddress;
 	int	mask;
 	
@@ -60,3 +58,48 @@ static	int setBit (int bitNumber, BYTE bitValue, BYTE *cache) {
 	
 }
 
+int	searchBitmapIndex(BYTE* buffer, int value){
+  int i;
+  bool booleanValue;
+
+  booleanValue = value == 0? false:true;
+
+  for(i = 0; i < bitmapInfo.indexBitmapSize; i++){
+    if (getBit(i,buffer) == booleanValue){
+      return i;
+    }
+  }
+
+  return ERROR;
+}
+
+int searchBitmapData(BYTE* buffer, int value){
+  int i;
+  bool booleanValue;
+
+  booleanValue = value == 0? false:true;
+
+  for(i = bitmapInfo.indexBitmapSize; i < bitmapInfo.indexBitmapSize + bitmapInfo.dataBitmapSize; i++){
+    if (getBit(i,buffer) == booleanValue){
+      return i;
+    }
+  }
+
+  return ERROR;
+}
+
+int	searchBitmap(int handle, int bitValue){
+  if(handle == BITMAP_INDEX)
+    return searchBitmapIndex(bufferBitmaps,bitValue);
+  
+  else //handle == BITMAP_DATA
+    return searchBitmapData(bufferBitmaps,bitValue); 
+}
+
+
+int setBitmap(int handle, int bitNumber, int bitValue){
+  if (handle == BITMAP_INDEX)
+    setBit(bitNumber,bitValue,bufferBitmaps);
+  else //handle == BITMAP_DATA
+    setBit(bitNumber + bitmapInfo.indexBitmapSize,bitValue,bufferBitmaps); 
+}
